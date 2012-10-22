6066d340cefc3f8828415a92336cf62497191c60
mattbellis <matthew.bellis@gmail.com>
2012-10-22 17:19:25 -0400

    Minor plotting changes.

--- research/plot_CogeNT_data.py
+++ research/plot_CogeNT_data.py
@@ -8,18 +8,20 @@ first_event = 2750361.2
 
 # We need to give the full path to the directory. This will obviously be 
 # different on your machine, so you will want to edit this by hand. 
-infile_name = '/Users/lm27apic/Documents/low_gain.txt'
+#infile_name = '/Users/lm27apic/Documents/low_gain.txt'
+#infile_name = '/home/bellis/matts-work-environment/PyROOT/CoGeNT/data/low_gain.txt'
+infile_name = '/home/bellis/matts-work-environment/PyROOT/CoGeNT/data/high_gain.txt'
 tdays,energies = cu.get_cogent_data(infile_name,first_event=first_event,calibration=0)
 
-index0 = energies>10.0
-index1 = energies<11.0
+index0 = energies>0.0
+index1 = energies<2.0
 print len(energies)
 print len(index0)
 print len(index1)
 index = index0*index1
 print index
-#x = energies[index]
-x = tdays[index]
+x = energies[index]
+#x = tdays[index]
 
 
 
@@ -29,8 +31,8 @@ plt.figure()
 # Note that we can set the number of bins (bins) and the x-axis range
 # (range) in the constructor.
 # plt.hist(energies,bins=100,range=(8,11))
-#plt.hist(x,bins=100,range=(8,11))
-plt.hist(x,bins=100,range=(0,900))
+plt.hist(x,bins=100,range=(0,2))
+#plt.hist(x,bins=100,range=(0,900))
 
 # Need to call the ``show" function to get the figure to pop up.
 plt.show()
