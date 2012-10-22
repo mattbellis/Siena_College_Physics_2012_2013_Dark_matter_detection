9b357d5b5058b9343aab1ef4e800c9bb8ef53f58
Laura Apicello <lm27apic@siena.edu>
2012-10-22 11:31:47 -0400

    small changes

--- research/plot_CogeNT_data.py
+++ research/plot_CogeNT_data.py
@@ -8,20 +8,43 @@ first_event = 2750361.2
 
 # We need to give the full path to the directory. This will obviously be 
 # different on your machine, so you will want to edit this by hand. 
-infile_name = '/Users/lm27apic/Documents/low_gain.txt'
-tdays,energies = cu.get_cogent_data(infile_name,first_event=first_event,calibration=0)
+infile_name = '/Users/lm27apic/Documents/Fall_2012/Dark_Matter_Research/dark_matter_data/low_gain.txt'
+tdays,energies = cu.get_cogent_data(infile_name,first_event=first_event,calibration=999)
 
-index0 = energies>10.0
-index1 = energies<11.0
+index0 = energies>0
+index1 = energies<0.2
 print len(energies)
 print len(index0)
 print len(index1)
 index = index0*index1
-print index
-#x = energies[index]
-x = tdays[index]
+# print index
+
+x = energies[index]
 
 
+# This will find the mean, sum, and stddev of an array z of some peak
+index2 = energies>0.106
+index3 = energies<0.116
+index4 = index2*index3
+z = energies[index4]
+t = tdays[index4]
+# plotting the peak
+plt.figure()
+plt.hist(z)
+# plotting the time for the peak
+plt.figure()
+plt.hist(t,bins=20)
+
+background = 20*len(z)
+mean = np.mean(z)
+stdev = np.std(z)
+summ = len(z)
+#summ = np.sum(z) - background
+
+print np.sum(z)
+print mean
+print stdev
+print summ
 
 # Make a figure on which to place the histogram
 plt.figure()
@@ -29,8 +52,14 @@ plt.figure()
 # Note that we can set the number of bins (bins) and the x-axis range
 # (range) in the constructor.
 # plt.hist(energies,bins=100,range=(8,11))
-#plt.hist(x,bins=100,range=(8,11))
-plt.hist(x,bins=100,range=(0,900))
+events = plt.hist(x,bins=300,range=(0,0.2))
+plt.xlabel('Amplitude')
+plt.ylabel('Number of Events')
+#plt.savefig('image~cal999_b200.png')
+
+#plt.hist(x,bins=100,range=(0,900))
+
+#print events
 
 # Need to call the ``show" function to get the figure to pop up.
 plt.show()
